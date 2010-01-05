wiseman/foursquare-python

commit fcc116dc9abf6ca39ac3cf02ef5fe301cf71eb04

Added friend and findfriends methods.  Added mechanism for an API method like friend/requests to be called by a python function named friend_requests.

foursquare.py
@@ -216,7 +216,35 @@ def_method('setpings',
 # Friend methods
 # --------------------
 
-# TBD
+def_method('friend_requests',
+           auth_required=True)
+
+def_method('friend_approve',
+           auth_required=True,
+           http_method='POST',
+           required=['uid'])
+
+def_method('friend_deny',
+           auth_required=True,
+           http_method='POST',
+           required=['uid'])
+
+def_method('friend_sendrequest',
+           auth_required=True,
+           http_method='POST',
+           required=['uid'])
+
+def_method('findfriends_byname',
+           auth_required=True,
+           required=['q'])
+
+def_method('findfriends_byphone',
+           auth_required=True,
+           required=['q'])
+
+def_method('findfriends_bytwitter',
+           auth_required=True,
+           required=['q'])
 
 
 # --------------------
@@ -411,7 +439,7 @@ class Foursquare:
         # Build the request.
         cred_url, cred_args, cred_headers = self.credentials.build_request(
             meta['http_method'],
-            meta['url_template'].substitute(method=method),
+            meta['url_template'].substitute(method=method.replace('_', '/')),
             kw,
             token=token)
 

tests/test_foursquare.py
@@ -4,7 +4,10 @@ import time
 import foursquare
 
 
-MY_COORDS=[34.09075, -118.27516]
+TEST_OAUTH = True
+
+MY_COORDS = [34.09075, -118.27516]
+
 
 class TestFoursquare(unittest.TestCase):
   def test_unauthenticated(self):
@@ -14,7 +17,6 @@ class TestFoursquare(unittest.TestCase):
     self.assertEqual(test_result['response'], 'ok')
 
     cities = fs.cities()
-    print cities
     self.failUnless('cities' in cities)
     self.failUnless(len(cities['cities']) > 0)
     self.failUnless(find_if(cities['cities'], lambda o: o['id'] == 34), "Where did LA go?")
@@ -29,8 +31,6 @@ class TestFoursquare(unittest.TestCase):
 
   def test_basic_auth(self):
     "Testing basic HTTP authentication."
-    username = raw_input('Enter your foursquare username: ')
-    password = raw_input('Enter your foursquare password: ')
     fs = foursquare.Foursquare(foursquare.BasicCredentials(username, password))
     test_result = fs.test()
     self.assertEqual(test_result['response'], 'ok')
@@ -43,6 +43,9 @@ class TestFoursquare(unittest.TestCase):
 
 
   def test_oauth(self):
+    if not TEST_OAUTH:
+      return
+    
     # Authorization dance.
     oauth_key = raw_input('Enter your foursquare oauth consumer key: ')
     oauth_secret = raw_input('Enter your foursquare oauth consumer secret: ')
@@ -75,6 +78,16 @@ class TestFoursquare(unittest.TestCase):
     venues = fs.venues(geolat=MY_COORDS[0], geolong=MY_COORDS[1], l=1)
     self.failUnless('groups' in venues)
     self.assertEqual(len(venues['groups'][0]['venues']), 1)
+
+
+  def test_friends(self):
+    "Testing friend methods."
+    fs = foursquare.Foursquare(foursquare.BasicCredentials(username, password))
+    self.failUnless('requests' in fs.friend_requests())
+    users = fs.findfriends_byname('william')
+    print len(users['users'])
+    self.failUnless('users' in users)
+    self.failUnless(len(users['users']) > 0)
     
 
 def find_if(objs, pred):
@@ -84,6 +97,12 @@ def find_if(objs, pred):
   return None
 
 
+
+username = None
+password = None
+
 if __name__ == '__main__':
-    unittest.main()
+  username = raw_input('Enter your foursquare username: ')
+  password = raw_input('Enter your foursquare password: ')
+  unittest.main()
     

commit 232dda7bcd0de9a15dc9bb9b6565245dcb840ccc

Oops.  Fixed broken oauth handling introduced in commit fcc116dc9abf6ca39ac3cf02ef5fe301cf71eb04

foursquare.py
@@ -88,7 +88,8 @@ FOURSQUARE_METHODS = {}
 
 def def_method(name, auth_required=False, server=API_SERVER,
                http_method="GET", optional=[], required=[],
-               returns=None, url_template=API_URL_TEMPLATE):
+               returns=None, url_template=API_URL_TEMPLATE,
+               namespaced=True):
     FOURSQUARE_METHODS[name] = {
         'server': server,
         'http_method': http_method,
@@ -97,6 +98,7 @@ def def_method(name, auth_required=False, server=API_SERVER,
         'required': required,
         'returns': returns,
         'url_template': url_template,
+        'namespaced': namespaced
         }
 
 
@@ -107,19 +109,22 @@ def def_method(name, auth_required=False, server=API_SERVER,
 def_method('request_token',
            server=OAUTH_SERVER,
            returns='oauth_token',
-           url_template=OAUTH_URL_TEMPLATE)
+           url_template=OAUTH_URL_TEMPLATE,
+           namespaced=False)
 
 def_method('authorize',
            server=OAUTH_SERVER,
            required=['token'],
            returns='request_url',
-           url_template=OAUTH_URL_TEMPLATE)
+           url_template=OAUTH_URL_TEMPLATE,
+           namespaced=False)
 
 def_method('access_token',
            server=OAUTH_SERVER,
            required=['token'],
            returns='oauth_token',
-           url_template=OAUTH_URL_TEMPLATE)
+           url_template=OAUTH_URL_TEMPLATE,
+           namespaced=False)
 
 
 # --------------------
@@ -460,11 +465,19 @@ class Foursquare:
             token = None
 
         # Build the request.
-        cred_url, cred_args, cred_headers = self.credentials.build_request(
-            meta['http_method'],
-            meta['url_template'].substitute(method=method.replace('_', '/')),
-            kw,
-            token=token)
+        if meta['namespaced']:
+            cred_url, cred_args, cred_headers = self.credentials.build_request(
+                meta['http_method'],
+                meta['url_template'].substitute(method=method.replace('_', '/')),
+                kw,
+                token=token)
+        else:
+            cred_url, cred_args, cred_headers = self.credentials.build_request(
+                meta['http_method'],
+                meta['url_template'].substitute(method=method),
+                kw,
+                token=token)
+            
 
         # If the return type is the request_url, simply build the URL and 
         # return it witout executing anything    